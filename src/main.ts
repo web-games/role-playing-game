b77b00ce17658c33eaed36bfd9c3917f160b0d16
zengweibin <zengweibin@vipkid.com.cn>
2019-08-06 10:27:54 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-interface StringValidator {
-  isAcceptable(s: string): boolean;
-}
-
-let lettersRegexp = /^[A-Za-z]+$/;
-let numberRegexp = /^[0-9]+$/;
-
-class LettersOnlyValidator implements StringValidator {
-  isAcceptable(s: string) {
-    return lettersRegexp.test(s);
+namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
   }
-}
 
-class ZipCodeValidator implements StringValidator {
-  isAcceptable(s: string) {
-    return s.length === 5 && numberRegexp.test(s);
+  const lettersRegexp = /^[A-Za-z]+$/;
+  const numberRegexp = /^[0-9]+$/;
+
+  export class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return lettersRegexp.test(s);
+    }
+  }
+
+  export class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return s.length === 5 && numberRegexp.test(s);
+    }
   }
 }
 
@@ -21,14 +23,13 @@ class ZipCodeValidator implements StringValidator {
 let strings = ["Hello", "98052", "101"];
 
 // Validators to use
-let validators: { [s: string]: StringValidator; } = {};
-validators["ZIP code"] = new ZipCodeValidator();
-validators["Letters only"] = new LettersOnlyValidator();
+let validators: { [s: string]: Validation.StringValidator; } = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
 
 // Show whether each string passed each validator
 for (let s of strings) {
   for (let name in validators) {
-    let isMatch = validators[name].isAcceptable(s);
-    console.log(`'${ s }' ${ isMatch ? "matches" : "does not match" } '${ name }'.`);
+    console.log(`"${ s }" - ${ validators[name].isAcceptable(s) ? "matches" : "does not match" } ${ name }`);
   }
 }
