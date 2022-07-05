6158a5753dd47c1b6d4cf8772f9f7a5b705f4693
bin.zeng <yy282371715@sina.cn>
2022-07-05 18:19:33 +0800

    update

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,32 +1,11 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require("copy-webpack-plugin")
+const {merge} = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.config')
 
-module.exports = {
+let webpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
-  entry: {
-    index: './src/main.ts',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        removeComments: false,
-        collapseWhitespace: false,
-        removeAttributeQuotes: false
-      }
-    }),
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -37,15 +16,10 @@ module.exports = {
     })
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
-    clean: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, './')
-    },
-    extensions: ['.ts', '.js', '.json']
+    publicPath: './'
   }
-}
+});
+
+// console.log(webpackConfig)
+
+module.exports = webpackConfig
