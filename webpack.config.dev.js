0513f30cd6bdc0986905af5401efc310249c9a0f
bin.zeng <yy282371715@sina.cn>
2022-07-05 18:06:54 +0800

    build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,8 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {merge} = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.config')
 
-module.exports = {
+module.exports = merge(baseWebpackConfig, {
   mode: 'development',
-  entry: {
-    index: './src/main.ts',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: "./",
@@ -22,28 +10,5 @@ module.exports = {
     port: 9000,
     hot: false,
     liveReload: false,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        removeComments: false,
-        collapseWhitespace: false,
-        removeAttributeQuotes: false
-      }
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, './')
-    },
-    extensions: ['.ts', '.js', '.json']
   }
-};
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  entry,
-  output,
-  resolve,
-  module,
-  plugins,
+  entry: {
+    index: './src/main.ts',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './')
+    },
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        removeComments: false,
+        collapseWhitespace: false,
+        removeAttributeQuotes: false
+      }
+    }),
+  ],
 }
